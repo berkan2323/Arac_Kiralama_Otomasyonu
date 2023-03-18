cf43fb45fd8b16312517c7bd56d179b3d0215448
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2023-03-18 22:28:32 +0300

    Add files via upload

---
+++ rent_a_car/Migrations/202206051323581_1.cs
@@ -0,0 +1,18 @@
+﻿namespace rent_a_car.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Musterilers", "EhliyetNo", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Musterilers", "EhliyetNo", c => c.Int(nullable: false));
+        }
+    }
+}
---
+++ rent_a_car/frmAracListesi.cs
@@ -0,0 +1,27 @@
+﻿using rent_a_car.ORM.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rent_a_car
+{
+    public partial class frmAracListesi : Form
+    {
+        public frmAracListesi()
+        {
+            InitializeComponent();
+        }
+
+        ProjectContext db = new ProjectContext();
+        private void frmAracListesi_Load(object sender, EventArgs e)
+        {
+            dg_arac_listesi.DataSource = db.Arabalars.ToList();
+        }
+    }
+}
